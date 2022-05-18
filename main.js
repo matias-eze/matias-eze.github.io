e791f1d6ce833a812865f552051895ff4c67b33e
matias-eze <94569583+matias-eze@users.noreply.github.com>
2022-05-17 21:59:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+nombre = document.getElementById(nombre);
+email = document.getElementById(email);
+msj = document.getElementById(msj);
+enviar = document.getElementById(enviar);
+check= document.getElementById(check);
+
