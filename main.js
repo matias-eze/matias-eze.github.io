f2a91cc0c83d037f5cfb510ee6123a94c95e02d3
matias-eze <94569583+matias-eze@users.noreply.github.com>
2022-05-18 20:46:57 -0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-nombre = document.getElementById(nombre);
-email = document.getElementById(email);
-msj = document.getElementById(msj);
-enviar = document.getElementById(enviar);
-check= document.getElementById(check);
-
